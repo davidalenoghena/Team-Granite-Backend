191eda76b4e996cdc8384ab25a229b65caf0e293
David Alenoghena <43188489+davidalenoghena@users.noreply.github.com>
2020-06-12 15:48:58 +0100

    Base url to Swagger Documentation

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ dotenv.config();
 
 
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocumentation));
+app.use('/', swaggerUi.serve, swaggerUi.setup(openApiDocumentation));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
